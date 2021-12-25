57ac086bdbda206e2b76c8aed6e24d263e219bb5
Al Snow <jasnow@hotmail.com>
2021-12-25 10:24:30 -0500

    Upgraded to Ruby 3.0.3 + bundler gem + edge rails

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -38,4 +38,5 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.active_record.legacy_connection_handling = false
+  config.cache_classes = false
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,44 +1,40 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141027235356) do
+ActiveRecord::Schema.define(version: 2014_10_27_235356) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "locations", force: true do |t|
-    t.float    "latitude"
-    t.float    "longitude"
-    t.string   "name"
-    t.string   "address"
-    t.string   "land_use_description"
-    t.string   "neighborhood_name"
-    t.boolean  "sidewalks"
-    t.string   "violations"
-    t.string   "lot_condition"
-    t.string   "structure_condition"
-    t.string   "digest_year"
-    t.string   "owner"
-    t.string   "tax_district"
-    t.string   "objectid_1"
-    t.string   "objectid"
-    t.string   "val_acres"
-    t.string   "structure_year"
-    t.string   "source"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean  "ebt"
-    t.string   "county"
+  create_table "locations", force: :cascade do |t|
+    t.float "latitude"
+    t.float "longitude"
+    t.string "name"
+    t.string "address"
+    t.string "land_use_description"
+    t.string "neighborhood_name"
+    t.boolean "sidewalks"
+    t.string "violations"
+    t.string "lot_condition"
+    t.string "structure_condition"
+    t.string "digest_year"
+    t.string "owner"
+    t.string "tax_district"
+    t.string "objectid_1"
+    t.string "objectid"
+    t.string "val_acres"
+    t.string "structure_year"
+    t.string "source"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "ebt"
+    t.string "county"
   end
 
 end
