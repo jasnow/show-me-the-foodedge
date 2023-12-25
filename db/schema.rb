4b95f4de25f11f30d059196aaa711fed9403a647
Al Snow <jasnow@hotmail.com>
2023-12-25 07:12:46 -0500

    new db/schema.rb file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2014_10_27_235356) do
+ActiveRecord::Schema[7.2].define(version: 2014_10_27_235356) do
   create_table "locations", force: :cascade do |t|
     t.float "latitude"
     t.float "longitude"
